fd307d90c7af41d2517d4822f87a3fec7fb4ba3f
Greg Martinez <greg.martinez44@gmail.com>
2022-08-28 09:38:59 -0700

    moved google sheet dash to parent directory

--- connections.py
+++ connections.py
@@ -1,26 +1,22 @@
 import pandas as pd
 
 
-def mysql_connection():
-    from python_mysql_connector.python_mysql_connector import common_queries as cq
-    return {
-        "all_books": cq.all_books(),
-        "completed_books": cq.completed_books(),
-        "all_purchases": cq.all_purchases(),
-        "unfinished_reads": cq.unfinished_reads(),
-        "started_reads": cq.started_reads()
-    }
+def mysql_connection(view_name):
+    from python_mysql_connector.python_mysql_connector import Database
+    with Database() as db:
+        return db.query(f"select * from {view_name}")
 
 
 def google_connection(sheet_id, sheet_name):
     from gpysheets.gpysheets.QuerySheet import QuerySheet
-    return QuerySheet.query_sheet(sheet_id, sheet_name)
+    query_result = QuerySheet.query_sheet(sheet_id, sheet_name)
+    return pd.DataFrame(query_result["data"], columns=query_result["columns"])
 
 
 if __name__ == "__main__":
-    # conns = google_connection(
-    #     "1zE89OFtETLOf8LFwYjcMrG0kRZ4I8WnSIgDgFT5nMbE", "f_books")
-    # print(conns["columns"])
+    conns = google_connection(
+        "1zE89OFtETLOf8LFwYjcMrG0kRZ4I8WnSIgDgFT5nMbE", "f_books")
+    print(conns.columns)
 
-    conns = mysql_connection()
-    print(conns["all_books"].columns)
+    # conns = mysql_connection("f_books")
+    # print(conns.columns)
